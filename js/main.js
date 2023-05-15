8a7feb1cb331475ccd6c7c5fd43f6aef9b46ed94
Vanessa <vanessa35lv@gmail.com>
2023-05-14 22:23:27 -0300

    salvando projeto crie sua arte

---
+++ js/main.js
@@ -0,0 +1,9 @@
+let nav = document.querySelector(".navbar");
+window.onscroll = function() {
+    if(document.documentElement.scrollTop > 50) {
+        nav.classList.add("header-scrolled");
+    }
+    else {
+        nav.classList.remove("header-scrolled");
+    }
+}
